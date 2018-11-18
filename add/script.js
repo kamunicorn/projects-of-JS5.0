fc1ed16b9cc6fe5b7824e379de9fe0e3812ed7b2
Vera <vera.kamunicorn@gmail.com>
2018-11-18 23:13:05 +1200

    Третий коммит - доп.задание

---
+++ add/script.js
@@ -0,0 +1,10 @@
+"use strict";
+    // Получаем число от пользователя
+let num = prompt('Введите 5-разрядное число:', 33721);
+let multiplication = num[0] * num[1] * num[2] * num[3] * num[4];
+console.log(multiplication);
+
+let exponenta = multiplication * multiplication * multiplication;
+console.log(multiplication + ' в третьей степени = ' + exponenta);
+let expString = exponenta+'';
+console.log('первые два числа exponenta: ' + expString[0] + expString[1]);
