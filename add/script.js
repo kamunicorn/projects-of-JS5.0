afb80cd2b12ca06443ce1a0bcf0bd5d6bfbdf327
Vera <vera.kamunicorn@gmail.com>
2018-11-21 22:09:55 +1200

    Второе ДЗ - циклы, условия

--- add/script.js
+++ add/script.js
@@ -1,10 +1,10 @@
 "use strict";
     // Получаем число от пользователя
-let num = prompt('Введите 5-разрядное число:', 33721);
-let multiplication = num[0] * num[1] * num[2] * num[3] * num[4];
+let num = prompt('Введите 5-разрядное число:', 33721),
+	multiplication = num[0] * num[1] * num[2] * num[3] * num[4];
 console.log(multiplication);
 
-let exponenta = multiplication * multiplication * multiplication;
+let exponenta = multiplication ** 3;
 console.log(multiplication + ' в третьей степени = ' + exponenta);
 let expString = exponenta+'';
 console.log('первые два числа exponenta: ' + expString[0] + expString[1]);
--- budget/script.js
+++ budget/script.js
@@ -1,10 +1,7 @@
 "use strict";
 
-let money,
-time;
-
-money = prompt('Ваш бюджет на месяц?');
-time = prompt('Введите дату в формате YYYY-MM-DD');
+let money = +prompt('Ваш бюджет на месяц?'),
+	time = prompt('Введите дату в формате YYYY-MM-DD');
 
 let appData = {
     budget: money,
@@ -14,15 +11,39 @@ let appData = {
     income: undefined,
     savings: false
 }
-console.log(appData);
+// console.log(appData);
 
-let answer1 = prompt('Введите обязательную статью расходов в этом месяце');
-let answer2 = + prompt('Во сколько обойдется?');
-let answer3 = prompt('Введите обязательную статью расходов в этом месяце');
-let answer4 = + prompt('Во сколько обойдется?');
+/*for (let i = 0; i < 2; i++) {
+	let key = prompt('Введите обязательную статью расходов в этом месяце'),
+	value = prompt('Во сколько обойдется?');
+		// Если строка (value) не является в точности числом, то результат будет NaN
+		// Функция isFinite(n) преобразует аргумент к числу и возвращает true, если это не NaN/Infinity
+	if (key != null && key != '' && value != null && value != '' && isFinite(value)) {
+		appData.expense[key] = value;
+	} else {
+		alert('Введена неверная пара ключ - значение:  "' + key + '" = "' + value + '"');
+	}
+}*/
 
-appData.expense[answer1] = answer2;
-appData.expense[answer3] = answer4;
+/*while (Object.keys(appData.expense).length < 2) {
+	let key = prompt('Введите обязательную статью расходов в этом месяце'),
+	value = prompt('Во сколько обойдется?');
+	if (key != null && key != '' && value != null && value != '' && isFinite(value)) {
+		appData.expense[key] = value;
+	} else {
+		alert('Введена неверная пара ключ - значение:  "' + key + '" = "' + value + '"');
+	}
+}*/
+do {
+	let key = prompt('Введите обязательную статью расходов в этом месяце'),
+	value = prompt('Во сколько обойдется?');
+	if (key != null && key != '' && value != null && value != '' && isFinite(value)) {
+		appData.expense[key] = value;
+	} else {
+		alert('Введена неверная пара ключ - значение:  "' + key + '" = "' + value + '"');
+	}
+} while (Object.keys(appData.expense).length < 2)
 
-let budgetOneDay = (appData.budget - answer2 - answer4) / 30;
+console.log(appData.expense);
+let budgetOneDay = appData.budget / 30;
 alert('Бюджет на 1 день: ' + budgetOneDay);
