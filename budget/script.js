5e97e16c9c21a655271604ae55cbb04da4311d32
Vera <vera.kamunicorn@gmail.com>
2018-11-22 22:56:42 +1200

    Третье ДЗ - функции

--- budget/script.js
+++ budget/script.js
@@ -1,17 +1,92 @@
-"use strict";
-
-let money = +prompt('Ваш бюджет на месяц?'),
-	time = prompt('Введите дату в формате YYYY-MM-DD');
+let money, time, budgetOneDay;
 
 let appData = {
-    budget: money,
-    timeData: time,
+    budget: undefined,
+    timeData: undefined,
     expense: {},
-    optionalExpense: undefined,
+    optionalExpense: [],
     income: undefined,
     savings: false
 }
-// console.log(appData);
+
+function getStartData() {
+	money = +prompt('Ваш бюджет на месяц?');
+	time = prompt('Введите дату в формате YYYY-MM-DD');
+	while (isNaN(money) || money == '' || money == null) {
+		money = +prompt('Ваш бюджет на месяц?');
+	}
+	appData.budget = money;
+    appData.timeData = time;
+}
+
+function getExpense() {
+	while (Object.keys(appData.expense).length < 2) {
+		let key = prompt('Введите обязательную статью расходов в этом месяце'),
+		value = prompt('Во сколько обойдется?');
+		if (key != null && key != '' && value != null && value != '' && isFinite(value)) {
+			appData.expense[key] = +value;
+		} else {
+			alert('Введена неверная пара ключ - значение:  "' + key + '" = "' + value + '"');
+		}
+	}
+}
+
+function detectDayBudget() {
+	budgetOneDay = Math.round(appData.budget / 30);
+	alert('Бюджет на 1 день: ' + budgetOneDay);
+}
+
+/*function detectLevel() {
+	if (budgetOneDay < 300) {
+		alert('Низкий уровень достатка :-(');
+	} else if (budgetOneDay < 1000) {
+		alert('Средний уровень достатка :-)');
+	} else if (budgetOneDay >= 1000) {
+		alert('Высокий уровень достатка ^_^');
+	} else {
+		alert('Неопознанный уровень достатка О_о')
+	}
+}*/
+
+function detectLevel() {
+	switch (budgetOneDay) {
+		case (budgetOneDay < 300):
+			alert('Низкий уровень достатка :-(');
+			break;
+		case (budgetOneDay < 1000):
+			alert('Средний уровень достатка :-)');
+			break;
+		case 1000: 
+		case (budgetOneDay > 1000):
+			alert('Высокий уровень достатка ^_^');
+			break;
+		default:
+			alert('Неопознанный уровень достатка О_о')
+			break;
+	}
+}
+
+function chooseOptExpenses() {
+	let i = 0;
+	while (i < 3) {
+		answer = prompt('Статья необязательных расходов?');
+		if (answer != null && answer != '' && !isFinite(answer)) {
+			appData.optionalExpense[i] = answer;
+			i++;
+		} else {
+			alert('Введено неверное значение: "' + answer + '"');
+		}
+	}
+}
+
+getStartData();
+getExpense();
+detectDayBudget();
+detectLevel();
+chooseOptExpenses();
+
+console.log(appData);
+
 
 /*for (let i = 0; i < 2; i++) {
 	let key = prompt('Введите обязательную статью расходов в этом месяце'),
@@ -24,8 +99,7 @@ let appData = {
 		alert('Введена неверная пара ключ - значение:  "' + key + '" = "' + value + '"');
 	}
 }*/
-
-/*while (Object.keys(appData.expense).length < 2) {
+/*do {
 	let key = prompt('Введите обязательную статью расходов в этом месяце'),
 	value = prompt('Во сколько обойдется?');
 	if (key != null && key != '' && value != null && value != '' && isFinite(value)) {
@@ -33,17 +107,4 @@ let appData = {
 	} else {
 		alert('Введена неверная пара ключ - значение:  "' + key + '" = "' + value + '"');
 	}
-}*/
-do {
-	let key = prompt('Введите обязательную статью расходов в этом месяце'),
-	value = prompt('Во сколько обойдется?');
-	if (key != null && key != '' && value != null && value != '' && isFinite(value)) {
-		appData.expense[key] = value;
-	} else {
-		alert('Введена неверная пара ключ - значение:  "' + key + '" = "' + value + '"');
-	}
-} while (Object.keys(appData.expense).length < 2)
-
-console.log(appData.expense);
-let budgetOneDay = appData.budget / 30;
-alert('Бюджет на 1 день: ' + budgetOneDay);
+} while (Object.keys(appData.expense).length < 2)*/
