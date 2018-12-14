a178b7f627c3cca2a0866a506c4619bf08748c53
Vera <vera.kamunicorn@gmail.com>
2018-12-14 23:25:19 +1200

    Ручная привязка контекста (инпут age) к функции showUser

---
+++ this/script.js
@@ -0,0 +1,23 @@
+let age = document.getElementById('age');
+
+let alex = new User('Ivanov', 'Alex');
+console.log(alex);
+
+showUser.apply(age, ['Ivanov', 'Alex']);
+showUser.apply(age, ['Alekseev', 'Ivan']);
+
+let result = showUserObj.bind(age);
+console.log(result(alex));
+
+function User(surname, name) {
+	this.surname = surname;
+	this.name = name;
+}
+
+function showUser(surname, name) {
+    alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+}
+
+function showUserObj(obj) {
+    return "Пользователь " + obj.surname + " " + obj.name + ", его возраст " + this.value;
+}
