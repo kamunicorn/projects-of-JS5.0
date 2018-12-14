af279eb9998d5c8e669e499e5df84cbe5f578bb2
Vera <vera.kamunicorn@gmail.com>
2018-12-14 22:11:38 +1200

    Добавила появление модального окна при нажатии на кнопку Узнать подробнее в первом блоке. И при появлении модалки анимация работает на кнопке-истонике, а при закрытии окна класс анимации убирается с кнопки-источника

--- Yoga/js/script.js
+++ Yoga/js/script.js
@@ -2,25 +2,22 @@
 
 window.addEventListener('DOMContentLoaded', function() {
 
+		// tabs switching
+
 	let headerOfTabs = document.querySelector('.info-header'),
 		tabs = document.querySelectorAll('.info-header-tab'),
-		tabContent = document.querySelectorAll('.info-tabcontent');
+		tabContent = document.querySelectorAll('.info-tabcontent'),
+		descriptionBtn = document.querySelectorAll('.description-btn'),
+		sourceOfClick;
 
-	function hideTabContent(n) {
-		for (let i = n; i < tabContent.length; i++) {
-			tabContent[i].classList.remove('show');
-			tabContent[i].classList.add('hide');
-		}
-	};
+	descriptionBtn.forEach(function(btn) {
+		btn.addEventListener('click', function() {
+			sourceOfClick = this;
+			showModal(this);
+		});
+	});
 
 	hideTabContent(1);
-
-	function showTabContent(n) {
-		if (tabContent[n].classList.contains('hide')) {
-			tabContent[n].classList.remove('hide');
-			tabContent[n].classList.add('show');
-		}
-	};
 
 	headerOfTabs.addEventListener('click', function(event) {
 		let target = event.target;
@@ -36,26 +33,38 @@ window.addEventListener('DOMContentLoaded', function() {
 		}
 	});
 
+	function hideTabContent(n) {
+		for (let i = n; i < tabContent.length; i++) {
+			tabContent[i].classList.remove('show');
+			tabContent[i].classList.add('hide');
+		}
+	};
+
+	function showTabContent(n) {
+		if (tabContent[n].classList.contains('hide')) {
+			tabContent[n].classList.remove('hide');
+			tabContent[n].classList.add('show');
+		}
+	};
+
 		// countdown timer
 
 	let deadline = '2019-01-01 00:00:00';
-	getTimeDifference(deadline);
 
 	function getTimeDifference(endtime) {
 		let d = Date.parse(endtime) - Date.parse(new Date());
 		if (d <= 0) {
 			return {
-			'total' : 0,
-			'seconds' : 0,
-			'minutes' : 0,
-			'hours' : 0
-		};
+				'total' : d,
+				'seconds' : 0,
+				'minutes' : 0,
+				'hours' : 0
+			};
 		}
 		let	seconds = Math.floor( (d/1000) % 60 ),
 			minutes = Math.floor( (d/1000/60) % 60 ),
 			hours = Math.floor( d / 1000/60/60 );
-		let days = Math.floor( hours / 24 );
-
+		
 		return {
 			'total' : d,
 			'seconds' : seconds,
@@ -84,25 +93,32 @@ window.addEventListener('DOMContentLoaded', function() {
 	}
 
 	setClock('timer', deadline);
+
+		// Modal
+
+	let more = document.querySelector('.more'),
+		overlay = document.querySelector('.overlay'),
+		popupClose = document.querySelector('.popup-close');
+
+	more.addEventListener('click', function() {
+		sourceOfClick = this;
+		showModal(this);
+	});
+
+	popupClose.addEventListener('click', function() {
+		overlay.style.display = 'none';
+		console.log(sourceOfClick);
+		sourceOfClick.classList.remove('more-splash');
+		document.body.style.overflow = '';
+	});
+
+	function showModal(source) {
+		source.classList.add('more-splash');
+		overlay.style.display = 'block';
+		document.body.style.overflow = 'hidden';
+	}
 });
 
-	// Modal
-
-let more = document.querySelector('.more'),
-	overlay = document.querySelector('.overlay'),
-	popupClose = document.querySelector('.popup-close');
-
-more.addEventListener('click', function() {
-	overlay.style.display = 'block';
-	this.classList.add('more-splash');
-	document.body.style.overflow = 'hidden';
-});
-
-popupClose.addEventListener('click', function() {
-	overlay.style.display = 'none';
-	more.classList.remove('more-splash');
-	document.body.style.overflow = '';
-});
 
 function addZero(digit) {
     return (digit < 10) ? '0' + digit : '' + digit;
