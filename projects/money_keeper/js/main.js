2448b141f92e696b6967092f7ca921e4f3300f28
Vera <vera.kamunicorn@gmail.com>
2018-12-31 16:47:09 +1200

    Доделала логику калькулятора, навела красоту

--- projects/money_keeper/js/main.js
+++ projects/money_keeper/js/main.js
@@ -1,5 +1,3 @@
-/* jshint esversion: 6 */
-/* jshint browser: true*/
 "use strict";
 
 let expensesInps = document.getElementsByClassName('expenses-item'),
@@ -35,170 +33,203 @@ let appData = {
     budget: undefined,
     timeData: undefined,
     expenses: {},
-    optExpense: {},
+    optExpense: [],
     income: [],
     savings: false,
     savingsSum: undefined,
     savingsPercent: undefined,
-
     countSavings: function() {
-        if (appData.savings) {
-            appData.monthIncome = appData.savingsSum/100/12 * appData.savingsPercent;
-            appData.yearIncome = appData.savingsSum/100 * appData.savingsPercent;
+        if (this.savings && this.savingsSum != undefined && this.savingsPercent != undefined) {
+            this.monthIncome = this.savingsSum/100/12 * this.savingsPercent;
+            this.yearIncome = this.savingsSum/100 * this.savingsPercent;
         } else {
-            console.log('Произошла какая-то ошибка при подсчете накоплений.')
+            this.monthIncome = '';
+            this.yearIncome = '';
         }
     }
-}
+};
     // полнота (не)обязательных расходов, проверяется в функции verifyDataCompleteness
-let expensesDataCompleteness = {},
-    optExpensesDataCompleteness = {},
-    allDataInputs = document.querySelectorAll('.data input');
-    
-    /*************************************************************************
+let expComplete = {},
+    optExpComplete = {};
+
+    // Все инпуты очищаются при запуске и перезапеске приложения
+    // Все кнопки отключаются, кноме стартовой
+    // Все результаты расчетов также очищаются
+let allDataInputs = document.querySelectorAll('.data input'),
+    allDataButtons = document.querySelectorAll('.data button'),
+    resultValues = [];
+
+document.querySelectorAll('.result-table div').forEach( (item) => {
+    if (/-value/.test(item.className)) {
+        resultValues.push(item);
+    }
+});
+
+        /*************************************************************************
         Обработчики событий на кнопки, инпуты - навешиваются по отдельности */
 
 window.addEventListener('DOMContentLoaded', function() {
-
-    // allDataButtons = document.querySelectorAll('.data button'),
-    allDataInputs.forEach((inp) => {inp.setAttribute('disabled', true);});
-
-    expensesBtn.setAttribute('disabled', true);
-    optExpensesBtn.setAttribute('disabled', true);
-    countBudgetBtn.setAttribute('disabled', true);
-
+    allDataInputs.forEach( (inp) => {inp.setAttribute('disabled', true);} );
+    startApp();
 });
 
     /* Кнопка Начать расчет: Получение значений бюджета (общий), строки даты, и последующая запись всего этого в поля справа */
-startBtn.addEventListener('click', function() {
-    let money,
-        dateStr;
+startBtn.addEventListener('click', () => {
+    let money, dateStr;
 
-    while (!isNumeric(money)) {
+    while (true) {
         money = prompt('Ваш бюджет на месяц?', '');
-        if (!isNumeric(money)) {
+        if (isContainOnlyDigits(money)) {
+            break;
+        } else {
             alert('Нужно ввести число.');
         }
     }
     money = +money;
     appData.budget = money;
 
-    while (!isDate(dateStr)) {
+    while (true) {
         dateStr = prompt('Введите дату в формате YYYY-MM-DD', '');
-        if (!isDate(dateStr)) {
+        if (isDate(dateStr)) {
+            break;
+        } else {
             alert('Нужно ввести дату в формате YYYY-MM-DD.');
         }
     }
     appData.timeData = new Date(dateStr);
-
     yearValue.value = appData.timeData.getFullYear();
     monthValue.value = appData.timeData.getMonth() + 1;
     dayValue.value = appData.timeData.getDate();
 
+    allDataInputs.forEach( (inp) => {inp.removeAttribute('disabled');} );
+    startApp();
     budgetResult.textContent = appData.budget;
+     
+});
 
-    allDataInputs.forEach(function(inp) {
-        inp.removeAttribute('disabled');
-        inp.value = '';
-    });
+function startApp() {
+    allDataButtons.forEach((btn) => {btn.setAttribute('disabled', true);});
+    startBtn.removeAttribute('disabled');
+    resultValues.forEach( (div) => {div.textContent = '';} );
+    allDataInputs.forEach( (inp) => {inp.value = '';} );
+
     savingsSumInp.setAttribute('disabled', true);
     savingsPercentInp.setAttribute('disabled', true);
     savingsChekbox.checked = false;
-    /*expensesBtn.removeAttribute('disabled');
-    optExpensesBtn.removeAttribute('disabled');
-    countBudgetBtn.removeAttribute('disabled');*/
-});
+}
 
     // Кнопка Рассчитать (дневной бюджет и уровень дохода)
-countBudgetBtn.addEventListener('click', function() {
+countBudgetBtn.addEventListener('click', () => {
     let budgetOneDay = 
         (appData.budget == undefined) ? 'Бюджет не задан!' :
-        (appData.expenses == {} || isEmpty(expensesResult.textContent)) ? 'Не заданы обязательные расходы!' :
+        (appData.expenses == {} || expensesResult.textContent == '') ? 'Не заданы обязательные расходы!' :
         Math.round((appData.budget - expensesResult.textContent) / 30);
 
     dayBudgetResult.textContent = budgetOneDay;
 
     levelResult.textContent = 
-        (budgetOneDay < 500) ? 'Низкий уровень достатка :-(':
-        (budgetOneDay < 1500) ? 'Средний уровень достатка :-)':
-        (budgetOneDay >= 1500) ? 'Высокий уровень достатка ^_^':
-        'Бюджет не задан!';
+        (budgetOneDay < 500) ? 'Низкий уровень достатка':
+        (budgetOneDay < 1500) ? 'Средний уровень достатка':
+        (budgetOneDay >= 1500) ? 'Высокий уровень достатка':
+        'Бюджет не задан, ошибка!';
 });
 
     // Кнопка Утвердить (обязательные расходы)
-expensesBtn.addEventListener('click', function () {
+expensesBtn.addEventListener('click', () => {
     appData.expenses = {};
+    let expensesSum = 0;
+
     for (let i = 0; i < expensesInps.length; i+=2) {
-        let expenseKey = expensesInps[i].value,
-            expenseValue = expensesInps[i+1].value;
-        if (isEmpty(expenseKey)) {
-            alert('Наименование (ключ) обязательных расходов не может быть пустым.');
-        } else if (!isContainOnlyRussianLetters(expenseKey)) {
-            alert('Наименование (ключ) обязательных расходов "' + expenseKey + '" должен содержать только русские буквы.');
-        } else if (!isNumeric(expenseValue)) {
-            alert('Цена (значение) обязательных расходов "' + expenseValue + '" должно быть числом.');
-        } else {
-            appData.expenses[expenseKey] = +expenseValue;
-        }
+        let key = expensesInps[i].value,
+            value = +expensesInps[i+1].value;
+        appData.expenses[key] = value;
+        expensesSum += value;
     }
-    if (Object.keys(appData.expenses).length == expensesInps.length/2) {
-        let expensesSum = 0;
-        for (let key in appData.expenses) {
-            expensesSum += appData.expenses[key];
-        };
-        expensesResult.textContent = expensesSum;
-        countBudgetBtn.removeAttribute('disabled');
-        console.log('Получены все значения обязательных расходов:');
-        console.log(appData.expenses);
-    } else {
-        console.log('Не все значения обязательных расходов получены.');
-        console.log(appData.expenses);
-    }
+    expensesResult.textContent = expensesSum;
+    countBudgetBtn.removeAttribute('disabled');
+    console.log('Получены все значения обязательных расходов:');
+    console.log(appData.expenses);
 });
 
     // Кнопка Утвердить (НЕобязательные расходы)
-optExpensesBtn.addEventListener('click', function() {
+optExpensesBtn.addEventListener('click', () => {
     let i = 0;
-    optExpensesResult.textContent = '';
-    optExpensesInps.forEach(function(item) {
-        let answer = item.value;
-        if (isEmpty(answer)) {
-            // ничего (потому что это необязательный расход)
-        } else if (!isContainOnlyRussianLetters(answer)) {
-            // alert('Значение необязательных расходов должно содержать только русские буквы.');
-            item.value = removeNotRussianLetters(answer, true).trim();
-        }
-        else {
+    appData.optExpense = [];
+    optExpensesInps.forEach((item) => {
+        let answer = removeNotRussianLetters(item.value, true).trim();
+        if (answer != '') {
             appData.optExpense[i] = answer;
-            optExpensesResult.textContent += answer + ' ';
             i++;
         }
     });
-    if (isEmpty(optExpensesResult.textContent)) {
-        optExpensesResult.textContent = '—';
-    }
+    optExpensesResult.textContent = (appData.optExpense.length == 0) ? '—' : appData.optExpense.join(' ');
 });
 
-    // Инпут - статьи возможного дохода
-incomeInp.addEventListener('input', function() {
-    // appData.income = [];
-    let items = incomeInp.value;
-    if (isEmpty(items)) {
-        // ничего (потому что это необязательный доход)
-        appData.income = [];
-    } else if (!isContainOnlyRussianLetters(items, true)) { // не только буквы, с учетом запятой. Строка должна состоять только из русских букв, с разделителем из запятой (пробел допускается)
-        incomeInp.value = removeNotRussianLetters(items, true).trim();
-        
-    } else {
-        appData.income = items.split(',');
-        // incomeInp.value = items.trim();
+    // Навесим на все инпуты для обязательных расходов событие, проверяющее их на правильную заполненность и разблокирующее кнопку Утвердить
+for (let i = 0; i < expensesInps.length; i++) {
+    if ( i % 2 == 0 ) { // четные инпуты - название расходов
+        expensesInps[i].addEventListener('input', (event) =>{
+            let key = removeNotRussianLetters(event.target.value).trim();
+            event.target.value = key;
+            expComplete[i] = (key == null || key == '') ? false : true;
+            
+            if (verifyDataCompleteness(expComplete, expensesInps.length, true)) {
+                expensesBtn.removeAttribute('disabled');
+            } else {
+                expensesBtn.setAttribute('disabled', true);
+            }
+            console.log(expComplete);
+        });
+    } else {    // нечетные инпуты - значение расходов
+        expensesInps[i].addEventListener('input', () =>{
+            let value = +removeNotDigits(event.target.value);
+            if ( value == 0) {
+                value = '';
+            }
+            event.target.value = value;
+            expComplete[i] = (value == null || value == '') ? false : true;
+
+            if (verifyDataCompleteness(expComplete, expensesInps.length, true)) {
+                expensesBtn.removeAttribute('disabled');
+            } else {
+                expensesBtn.setAttribute('disabled', true);
+            }
+            console.log(expComplete);
+        });
     }
+}
+
+    // Навесим на все инпуты для необязательных расходов событие, проверяющее их на правильную заполненность и разблокирующее кнопку Утвердить
+for (let i = 0; i < optExpensesInps.length; i++) {
+    optExpensesInps[i].addEventListener('input', (event) => {
+        let key = removeNotRussianLetters(event.target.value).trim();
+        event.target.value = key;
+        
+        optExpComplete[i] = (key == null || key == '') ? false : true;
+            // нестрогая проверка (не передаем strictly)
+        if (verifyDataCompleteness(optExpComplete, optExpensesInps.length)) {
+            optExpensesBtn.removeAttribute('disabled');
+        } else {
+            optExpensesBtn.setAttribute('disabled', true);
+        }
+        console.log(optExpComplete);
+    });
+}
+
+    // Инпут - статьи возможного дохода
+incomeInp.addEventListener('input', () => {
+        // только русские буквы, с учетом запятой и пробела
+    let items = removeNotRussianLetters(incomeInp.value, true).trim();
+    incomeInp.value = items;
+    
+    appData.income = (items == '') ? [] : items.split(',');
     incomeResult.textContent = (appData.income.length == 0) ? '—' : appData.income.join('; ');
 });
 
     // Щелканье по чекбоксу Есть ли накопления
-savingsChekbox.addEventListener('change', function() {
+savingsChekbox.addEventListener('change', () => {
     appData.savings = savingsChekbox.checked;
+
     if (appData.savings) {
         savingsSumInp.removeAttribute('disabled');
         savingsPercentInp.removeAttribute('disabled');
@@ -212,39 +243,38 @@ savingsChekbox.addEventListener('change', function() {
     }
     savingsSumInp.value = '';
     savingsPercentInp.value = '';
+    setSavingsResult();
 });
 
     // Ввод значений в поле Сумма (накопления)
-savingsSumInp.addEventListener('input', function() {
-    let savingsSumValue = savingsSumInp.value;
-    if (!isContainOnlyDigits(savingsSumValue)) {
-        // alert('Должно быть введено число - сумма накоплений.');
-        savingsSumInp.value = savingsSumValue = removeNotDigits(savingsSumValue);
-    }
-    appData.savingsSum = (!isEmpty(savingsSumValue)) ? savingsSumValue : undefined;
-
-    if (appData.savingsPercent != undefined && appData.savingsSum != undefined) {
-        appData.countSavings();
-        monthSavingsResult.textContent = appData.monthIncome.toFixed(2);
-        yearSavingsResult.textContent = appData.yearIncome.toFixed(2);
-    }
+savingsSumInp.addEventListener('input', () => {
+    let savingsSumValue = removeNotDigits(savingsSumInp.value);
+    savingsSumInp.value = savingsSumValue;
+    
+    appData.savingsSum = (savingsSumValue != '') ? savingsSumValue : undefined;
+    setSavingsResult();
 });
 
     // Ввод значений в поле Процент (накопления)
-savingsPercentInp.addEventListener('input', function() {
-    let savingsPercentValue = savingsPercentInp.value;
-    if (!isNumeric(savingsPercentValue)) {
-        // alert('Должно быть введено число - процент (для накоплений).');
-        savingsPercentInp.value = savingsPercentValue = removeNotDigits(savingsPercentValue);
-        // savingsPercentInp.value = appData.savingsPercent;
-    }
-    appData.savingsPercent = (!isEmpty(savingsPercentValue)) ? savingsPercentValue : undefined;
-    if (appData.savingsPercent != undefined && appData.savingsSum != undefined) {
-        appData.countSavings();
+savingsPercentInp.addEventListener('input', () => {
+    let savingsPercentValue = removeNotDigits(savingsPercentInp.value);
+    savingsPercentInp.value = savingsPercentValue;
+    
+    appData.savingsPercent = (savingsPercentValue != '') ? savingsPercentValue : undefined;
+    setSavingsResult();
+});
+
+function setSavingsResult() {
+    appData.countSavings();
+    
+    if (appData.savings && appData.savingsSum != undefined && appData.savingsPercent != undefined) {
         monthSavingsResult.textContent = appData.monthIncome.toFixed(2);
         yearSavingsResult.textContent = appData.yearIncome.toFixed(2);
+    } else {
+        monthSavingsResult.textContent = '—';
+        yearSavingsResult.textContent = '—';
     }
-});
+}
 
 
     /****************************************************************
@@ -259,7 +289,7 @@ function isContainDigit(str) {  // проверка строки на содер
 }
 
 function isContainOnlyRussianLetters(str, comma) {  // проверка строки на содержание в ней только русских букв, пробела (и запятой)
-    if (isEmpty(str)) {
+    if (str == null || str == '') {
         return false;
     }
     if (comma) {    // с учетом запятой (разделитель)
@@ -270,19 +300,19 @@ function isContainOnlyRussianLetters(str, comma) {  // проверка стро
 }
 
 function isContainOnlyDigits(str) {  // проверка строки на содержание в ней только цифр (?и точки)
-    if (isEmpty(str)) {
+    if (str == null || str == '') {
         return false;
     }
     return !/[\D]/gi.test(str);   
 }
 
-function removeNotDigits(str) {
+function removeNotDigits(str, point) {
     return str.replace(/[\D]/gi, '');
 }
 
 function removeNotRussianLetters(str, comma) {
     if (comma) {    // с учетом запятой (разделитель)
-        return str.replace(/[^а-я, ]/gi, '');
+        return str.replace(/[^а-я,]/gi, '');
     }
     else {
         return str.replace(/[^а-я]/gi, '');
@@ -294,17 +324,43 @@ function isEmpty(str) { // не пропускаем пустые строки
 }
     /* проверка, можно ли получить из строки дату, не проверяет на соответствие календарю. То есть, если ввели '2018-02-30', то дата будет преобразована в '2018-03-02'. Также даже короткие и большие числа могут быть преобразованы в дату, например 10, 50, 23456 и т.д. */
 function isDate(str) {  
-    return !((new Date(str)) == 'Invalid Date') && !isEmpty(str);
+    if (new Date(str) == 'Invalid Date') {
+        return false;
+    } else if (str == null || str == '') {
+        return false;
+    } else {
+        return isValidDate(str);
+    }
 }
 
-    /* проверяет массив из булевых знаений умножением */
-function verifyDataCompleteness(arr, len) {
-    let result = true;
-    if (len != Object.keys(arr).length) {
+    // Проверка даты в целом на существование
+function isValidDate(str) {
+    let s = str.split('-', 3);
+    if (s.length != 3) {
         return false;
     }
-    for (let i = 0; i < len; i++) {
-        result *= arr[i];
-    };
-    return !!result;
+    let d = new Date(parseInt(s[0]), parseInt(s[1])-1, parseInt(s[2]));
+
+    if (Object.prototype.toString.call(d) === "[object Date]") 
+        { if ((!isNaN(d.getTime())) && (d.getDate() == parseInt(s[2])) && (d.getMonth() == (parseInt(s[1]) - 1))) {
+            return true;
+        }
+    }
+    return false;
+}
+
+    /* проверяет массив из булевых знаений, strictly - строгая проверка */
+function verifyDataCompleteness(obj, len, strictly) {
+    let arr = Object.keys(obj).map(function (key) { return obj[key]; });
+    if (strictly) {
+        if (len != arr.length) {
+            return false;
+        }
+        return arr.every( (item) => {return item == true;});
+    } else {
+        if (arr.length == 0) {
+            return false;
+        }
+        return arr.some( (item) => {return item == true;});
+    }
 }
